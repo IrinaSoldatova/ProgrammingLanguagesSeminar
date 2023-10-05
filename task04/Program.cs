fa0685a7b9fce87936ff21a4c2db5c789f39c92f
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-05 14:12:17 +0300

    add task 07

--- task04/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿System.Console.WriteLine("Введите число: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-for(int i = 0-userNumber; i<= userNumber; i++){
-System.Console.Write(i+" ");
-}
---
+++ task07/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+System.Console.WriteLine();
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
+System.Console.WriteLine();
