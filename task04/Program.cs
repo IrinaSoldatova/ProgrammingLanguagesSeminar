c75d0595447d6507d1f689e39122befa295172cd
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-05 12:18:46 +0300

    add task 04

---
+++ task04/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+for(int i = 0-userNumber; i<= userNumber; i++){
+System.Console.Write(i+" ");
+}
