cd93b5be3e3ce92d86229bc8200337271bee9ce8
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-05 11:41:43 +0300

    add task 02

---
+++ task02_дни_недели/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+
+if (day == 1)
+{
+    System.Console.WriteLine("Сегодня понедельник");
+}
+if (day == 2)
+{
+    System.Console.WriteLine("Сегодня вторник");
+}
+if (day == 3)
+{
+    System.Console.WriteLine("Сегодня среда");
+}
+if (day == 4)
+{
+    System.Console.WriteLine("Сегодня четверг");
+}
+if (day == 5)
+{
+    System.Console.WriteLine("Сегодня пятница");
+}
+if (day == 6)
+{
+    System.Console.WriteLine("Сегодня суббота");
+}
+if (day == 7)
+{
+    System.Console.WriteLine("Сегодня воскресенье");
+}
+else
+System.Console.WriteLine("Нет такого дня недели");
