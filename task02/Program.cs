54f6c2234c950ed936c041939979b3e1f4d02290
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-08 14:00:53 +0300

    исправления

--- task02/Program.cs
+++ task02/Program.cs
@@ -10,11 +10,11 @@ int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 if(firstNumber < secondNumber)
 {
-      System.Console.WriteLine("Первое число `" + firstNumber +"` меньше чем второе число `" + secondNumber +"`");
+      System.Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");
 }
 else if(firstNumber == secondNumber)
 {
-      System.Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
+      System.Console.WriteLine($"Введенные числа равны {firstNumber}");
 }
 else
-System.Console.WriteLine("Первое число `" + firstNumber + "` больше чем второе число `" + secondNumber +"`");
+System.Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
