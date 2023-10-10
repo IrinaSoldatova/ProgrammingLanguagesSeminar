d60f081d811c7c5a07bb61290e942fdf3a874ec0
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-10 17:22:44 +0300

    task 03

--- task03/Program.cs
+++ task03/Program.cs
@@ -14,27 +14,27 @@ if (day == 1)
 {
     System.Console.WriteLine("Сегодня понедельник");
 }
-if (day == 2)
+else if (day == 2)
 {
     System.Console.WriteLine("Сегодня вторник");
 }
-if (day == 3)
+else if (day == 3)
 {
     System.Console.WriteLine("Сегодня среда");
 }
-if (day == 4)
+else if (day == 4)
 {
     System.Console.WriteLine("Сегодня четверг");
 }
-if (day == 5)
+else if (day == 5)
 {
     System.Console.WriteLine("Сегодня пятница");
 }
-if (day == 6)
+else if (day == 6)
 {
     System.Console.WriteLine("Сегодня суббота");
 }
-if (day == 7)
+else if (day == 7)
 {
     System.Console.WriteLine("Сегодня воскресенье");
 }
