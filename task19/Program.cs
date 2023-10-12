47f997458cb866496532dcfea9348e2a189d79d3
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-12 23:18:24 +0300

    task 19 еще решение

--- task19/Program.cs
+++ task19/Program.cs
@@ -16,17 +16,14 @@ if (10000 > userNumber || userNumber > 99999)
     return;
 }
 
-int number = userNumber;
+
 int reverse = 0;
 
-while (number > 0)
+for (int number = userNumber; number > 0; number = number / 10)
 {
     int remain = number % 10;
     reverse = reverse * 10 + remain;
-    number = number / 10;
 }
-System.Console.WriteLine(reverse);
-
 if(userNumber == reverse)
 {
     System.Console.WriteLine("Чиcло является палиндромом");
@@ -38,7 +35,44 @@ else
 }
 
 
-/* Второй способ
+
+
+
+
+/* второй способ
+
+System.Console.Write("Введите пятизначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (10000 > userNumber || userNumber > 99999)
+{
+    System.Console.WriteLine("Число не пятизначное");
+    return;
+}
+
+int number = userNumber;
+int reverse = 0;
+
+while (number > 0)
+{
+    int remain = number % 10;
+    reverse = reverse * 10 + remain;
+    number = number / 10;
+}
+
+if(userNumber == reverse)
+{
+    System.Console.WriteLine("Чиcло является палиндромом");
+
+}
+else
+{
+    System.Console.WriteLine("Не является палиндромом");
+}
+*/
+
+
+/* третий способ
 
 System.Console.Write("Введите пятизначное число: ");
 int userNumber = Convert.ToInt32(Console.ReadLine());
