eb28d839f8f803f8507a6664f1e2af96f767791c
Irina Soldatova <soldatovaik@yandex.ru>
2023-10-17 20:49:38 +0300

    29

--- task29/Program.cs
+++ task29/Program.cs
@@ -16,15 +16,18 @@ void FillArray(int[] size)
 
 void PrintArray(int[] arr)
 {
+    Console.ForegroundColor=ConsoleColor.Green;//изменить цвет вывода
     System.Console.Write("[");
     for (int j = 0; j < arr.Length - 1; j++)
     {
+        Thread.Sleep(100);
         System.Console.Write(arr[j]);
         if (j < arr.Length - 1)
             System.Console.Write(", ");
     }
     System.Console.Write($"{arr[arr.Length - 1]}");
     System.Console.Write("]");
+    Console.ResetColor();//после вывода вернуть обратно белый цвет
 }
 
 System.Console.Write("Задайте размер массива: ");
@@ -66,4 +69,41 @@ System.Console.Write("Задайте размер массива: ");
 int userSize = Convert.ToInt32(Console.ReadLine());
 int[] userArray = FillArray(userSize);
 PrintArray(userArray);
+*/
+
+/* с запросом диапозона и размера массива от пользователя и рандомным методом
+Console.Clear();
+
+int[] GetArray(int start, int end, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(start, end+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+        System.Console.Write(", ");//[5, 7, 1, 4]
+    }
+    System.Console.Write("]");
+}
+
+System.Console.WriteLine("Задай диапазон массива от:");
+int userStart = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задай диапазон массива до:");
+int userEnd = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задай размер массива:");
+int userSize = Convert.ToInt32(Console.ReadLine());
+
+int[] userArray = GetArray(userStart, userEnd, userSize);
+PrintArray(userArray);
+
 */
